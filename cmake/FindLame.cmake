b997635dbc0b16e512d859334616e7e5ba0aa1af
Arthur Taylor <art@ified.ca>
2020-10-11 23:20:06 -0700

    mpeg: Fixes for CI builds

--- cmake/FindLame.cmake
+++ cmake/FindLame.cmake
@@ -24,6 +24,15 @@ find_library (LAME_LIBRARY
 		mp3lame_static
 		libmp3lame
 		libmp3lame_static
+		libmp3lame-static
+	HINTS
+		${LAME_ROOT}
+	)
+
+find_library (LAME_HIP_LIBRARY
+	NAMES
+		mpghip-static
+		libmpghip-static
 	HINTS
 		${LAME_ROOT}
 	)
@@ -38,16 +47,20 @@ find_package_handle_standard_args (Lame
 	)
 
 if (LAME_FOUND)
-	set (LAME_LIBRARIES ${LAME_LIBRARY})
+	set (LAME_LIBRARIES ${LAME_LIBRARY} ${LAME_HIP_LIBRARY})
 	set (LAME_INCLUDE_DIRS ${LAME_INCLUDE_DIR})
 
 	if (NOT TARGET Lame::Lame)
 		add_library (Lame::Lame UNKNOWN IMPORTED)
 		set_target_properties (Lame::Lame PROPERTIES
 			INTERFACE_INCLUDE_DIRECTORIES "${LAME_INCLUDE_DIRS}"
-			IMPORTED_LOCATION "${LAME_LIBRARIES}"
+			IMPORTED_LOCATION "${LAME_LIBRARY}"
 		)
+		if (LAME_HIP_LIBRARY)
+			set_property (TARGET Lame::Lame APPEND PROPERTY
+				INTERFACE_LINK_LIBRARIES "${LAME_HIP_LIBRARY}")
+		endif ()
 	endif ()
 endif ()
 
-mark_as_advanced(LAME_INCLUDE_DIR LAME_LIBRARY)
+mark_as_advanced(LAME_INCLUDE_DIR LAME_LIBRARY LAME_HIP_LIBRARY)
